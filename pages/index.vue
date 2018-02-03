c1c2a8bf499f4b13eb87cd7c8ff38b36151d79cb
Jason Kende <jk@inbox.io>
2018-02-03 12:45:05 -0500

    Blanked the default splash

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        node-nyc-preview
-      </h1>
-      <h2 class="subtitle">
-        A decentralized crypto lounge.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <!-- nothing to see here -->
   </section>
 </template>
 
