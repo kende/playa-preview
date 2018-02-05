7b2e332212d96c37a047bf4c34bdb6be83337396
Bearies <bearies@thicketlabs.com>
2018-02-05 15:39:34 -0500

    make og:image correct

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,18 +8,13 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'A decentralized crypto lounge.' },
-      { property: 'og:image', content: 'http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg' }
+      { property: 'og:image', content: 'https://node.nyc/_nuxt/img/glitch-bk.6cacbb8.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/node-favicon.png' },
       { href: 'https://fonts.googleapis.com/css?family=Archivo+Black', rel: 'stylesheet'}
     ]
   },
-// <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-// <meta property="og:type"               content="article" />
-// <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-// <meta property="og:description"        content="How much does culture influence creative thinking?" />
-// <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
   /*
   ** Customize the progress bar color
   */
