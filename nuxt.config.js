0705341ee360f78871149e471d689301aa5c2a8b
Bearies <bearies@thicketlabs.com>
2018-02-05 15:14:24 -0500

    update meta image url

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'A decentralized crypto lounge.' },
-      { property:"og:image", content:"https://node.nyc" + "~/assets/node-fb-img.png" }
+      { property: 'og:image', content: 'https://node.nyc/assets/node-fb-img.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/node-favicon.png' },
