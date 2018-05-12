cd21a5cb9ad4f9d093b4e952859326ce2055103b
Bearies <bearies@byzantine.network>
2018-05-12 18:12:59 -0400

    update page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,186 @@
 <template>
   <div class="landing">
-    <div class="landing-header">
-      <div class="brand">
+    <div class="banner-wrapper">
+      <!-- <div class="brand">
         <div class="thumbnail"><img src="~/assets/playa-fbimage.png"></div>
-        <!-- <nuxt-link to="/">
-          <img class="logo" src="~/assets/node_logo.png" alt="brand logo">
-        </nuxt-link> -->
-        <!-- <ul class="navbar">
-          <li class="navItem"><nuxt-link to="/">Home</nuxt-link></li>
-          <li class="navItem"><nuxt-link to="/about">About</nuxt-link></li>
-          <li class="navItem"><nuxt-link to="/about">Characters</nuxt-link></li>
-          <li class="navItem"><nuxt-link to="/about">Badges</nuxt-link></li>
-        </ul> -->
-      </div>
+      </div> -->
 
-      <div class="text">
-        <div class="page-title">Playa</div>
-        <div class="page-subtitle">Reserve your chance to join the origin quest now!</div>
-        <div class="apply"><a class="apply-btn" href="#apply-form">Ready playa?</a></div>
+      <div class="banner">
+        <!-- <canvas id="canvas"></canvas> -->
+        <img class="banner-image" src="~/assets/banner-image.png" alt="banner image">
+        <div class="page-title">PLAYA</div>
       </div>
     </div>
-    <div class="summary">
+
+    <div class="welcome-unicorn">
+      <img class="cloud" src="~/assets/cloud.png" alt="cloud">
+      <div class="unicorn-wrap">
+        <img class="unicorn" src="~/assets/unicorn.png" alt="unicorn">
+        <div class="dialog-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">Hi! I am 3.14159 the Univorn, but you can call me PI!</p>
+          <p class="dialog-p">I live on planet Playa in the Byzantine Multiverse, let me show you around!</p>
+        </div>
+      </div>
+    </div>  
+
+    <div class="explaining-unicorn">
+      <img class="cloud" src="~/assets/cloud.png" alt="cloud">
+      <div class="unicorn-wrap">
+        <img class="unicorn" src="~/assets/unicorn.png" alt="unicorn">
+        <div class="dialog-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">Here on Playa our favorite things to do are build things with our friends, learn new skills, battle Lucids, and go on Quests!</p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cloud-layer"><img class="cloud-single" src="~/assets/cloud-single.png" alt="cloud single"></div>
+
+    <div class="device-unicorn">
+      <img class="cloud" src="~/assets/cloud.png" alt="cloud">
+      <img class="cloud-single" src="~/assets/cloud-single.png" alt="cloud single">
+      <div class="unicorn-wrap">
+        <img class="unicorn" src="~/assets/unicorn.png" alt="unicorn">
+        <div class="dialog-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">We exist in a mobile game coming out on IOS and Android and assisted by blockchain technology. That makes us immortal! Once you create a character you can change it, but we never disappear, in fact we can play in other game worlds on the same blockchain!</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="unicorn-with-whale">
+      <div class="unicorn-wrap"><img class="unicorn" src="~/assets/unicorn.png" alt="unicorn"></div>
+      <div class="dialog-boxes">
+        <div class="unicorn-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">Hi Whaley! How did you get 13 hearts??</p>
+        </div>
+        <div class="whale-box">
+          <div class="dialog-name">WHALEY THE SHARK</div>
+          <p class="dialog-p">I make healthy food in the Cooking tab and make sure to exercise in the Movement Space! It's great because the more I go there to dance the better I get, soon I'll be able to earn Bright for my performances! I better get back to meditating!</p>
+        </div>
+      </div>
+      <div class="whale-wrap">
+        <div class="hexagons">
+          <div class="outer"></div>
+          <div class="inner"></div>
+          <div class="hexagons-title">STATS</div>
+          <p class="hexagons-text">Baby Whale Shark</p>
+          <p class="hexagons-text">Likes shells & singing</p>
+          <p class="hexagons-text">17 weeks old</p>
+          <p class="hexagons-text">13 hearts</p>
+        </div>
+        <img class="whale" src="~/assets/whaley-water.png" alt="whale in the water">
+      </div>
+    </div>  
+
+    <div class="unicorn-with-human">
+      <div class="unicorn-wrap"><img class="unicorn" src="~/assets/unicorn.png" alt="unicorn"></div>
+      <div class="dialog-boxes">
+        <div class="unicorn-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">Hi Rifka! I'm showing new friends around! Can you tell them about your camp setup?</p>
+        </div>
+        <div class="whale-box">
+          <div class="dialog-name">RIFKA THE HUMAN</div>
+          <p class="dialog-p">Sure, for now I am just setting up my personal tent with some items I have gained in quests, but soon I'll add more land to my Camp and start building a guild camp! I can't wait! Once I do that I can invite my friends to live in my camp with mw and we can build things together like art pieces and cars!</p>
+        </div>
+      </div>
+      <div class="whale-wrap">
+        <img class="whale" src="~/assets/rifka.png" alt="whale in the water">
+      </div>
+    </div>  
+
+    <div class="unicorn-with-other">
+      <div class="unicorn-wrap"><img class="unicorn" src="~/assets/unicorn.png" alt="unicorn"></div>
+      <div class="dialog-boxes">
+        <div class="unicorn-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">Hey y'all! What's new?</p>
+        </div>
+        <div class="whale-box">
+          <div class="dialog-name">JOHN THE LION</div>
+          <p class="dialog-p">Hi, PI! We were just discussing our latest adventures! I have been working on my fire spinning and my business! I have a new shop down in the marketplace! I sell fire items and weapons for fighting Lucids!</p>
+        </div>
+        <div class="whale-box">
+          <div class="dialog-name">CHRIS THE ASTRONAUT</div>
+          <p class="dialog-p">And I have been tring to make it to the next plannet! It has taken a lot of Quests to build my spaceship and gather gear but I am close!</p>
+        </div>
+      </div>
+      <div class="whale-wrap">
+        <img class="whale" src="~/assets/lion.png" alt="whale in the water">
+        <br>
+        <img class="whale" src="~/assets/astronaut.png" alt="whale in the water">
+      </div>
+    </div>
+
+    <div class="unicorn-with-rothko">
+      <div class="unicorn-wrap"><img class="unicorn" src="~/assets/unicorn.png" alt="unicorn"></div>
+      <div class="dialog-boxes">
+        <div class="unicorn-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">Oh Hi Rothko!</p>
+        </div>
+        <div class="whale-box">
+          <div class="dialog-name">ROTHKO THE DOG</div>
+          <p class="dialog-p">BARK!</p>
+        </div>
+      </div>
+      <div class="whale-wrap">
+        <img class="whale" src="~/assets/rothko.png" alt="whale in the water">
+      </div>
+    </div>  
+
+    <div class="unicorn-with-team">
+      <div class="unicorn-wrap">
+        <div class="unicorn-box">
+          <div class="dialog-name">PI THE UNICORN</div>
+          <p class="dialog-p">Hey ByzanTeam! There are the people behind Byzantine! They are designing and coding Playa and making building the smart contracts and tokens that run it!</p>
+        </div>
+        <img class="unicorn" src="~/assets/unicorn.png" alt="unicorn">
+      </div>
+      <div class="team-wrap">
+
+        <div class="name-row">
+          <div class="name-column">
+            <div class="team-member">JASON</div>
+            <div class="team-desc">Community <br>& Coding</div>
+          </div>
+          <div class="name-column">
+            <div class="team-member">NADIA</div>
+            <div class="team-desc">Data science <br>& AI</div>
+          </div>
+          <div class="name-column">
+            <div class="team-member">SAM</div>
+            <div class="team-desc">Design <br>& Gaming</div>
+          </div>
+        </div>
+        <img class="whale" src="~/assets/team.png" alt="whale in the water">
+        <div class="name-row left">
+          <div class="name-column">
+            <div class="team-member">MICHAEL</div>
+            <div class="team-desc">Community <br>& Operations</div>
+          </div>
+          <div class="name-column">
+            <div class="team-member">ED</div>
+            <div class="team-desc">Character Design <br>& Game Dev</div>
+          </div>
+          <div class="name-column">
+            <div class="team-member">PAUL</div>
+            <div class="team-desc">Gaming & <br> Blockchain Dev</div>
+          </div>
+          <div class="name-column">
+            <div class="team-member">BEARIES</div>
+            <div class="team-desc">Front End & Blockchain Dev</div>
+          </div>
+        </div>
+
+      </div>
+    </div>  
+
+    <!-- ---------------- -->
+    <!-- <div class="summary">
       <div class="title">What is Playa?</div>
       <div class="subtitle">Playa is...</div>
     </div>
@@ -48,7 +208,7 @@
           </ul>
         </div>
       </div>
-    </div>
+    </div> -->
     <!-- <div class="benefits">
       <div class="benefits-container">
         <div class="title">BENEFITS</div>
@@ -61,10 +221,8 @@
         </ul>
       </div>
     </div> -->
-    <div class="apply-form" id="apply-form">
-      <!-- <div class="title">APPLY TO NODE</div> -->
+    <!-- <div class="apply-form" id="apply-form">
       <div class="subtitle">Reserve your chance to join the origin quest now!</div>
-      <!-- <div class="apply"><a class="apply-btn" href="https://byzantine.typeform.com/to/fXpCQa" target="_blank">Ready playa?</a></div> -->
       <div class="subscription-form">
         <div class="subscription-group">
           <input type="email" placeholder="email" @keypress='emailChange' v-model='email' required>
@@ -79,43 +237,10 @@
           <button class='close-btn' @click='closePopup'>OK</button>
         </div>
       </div>
-      <!-- <div class="apply-btn" target="_blank"><a href="https://byzantine.typeform.com/to/fXpCQa">APPLY</a></div> -->
-      <!-- <form>
-        <div class="row">
-          <div class="column first-name input-group" label="First name">
-            <input class="input-box" type="text">
-          </div>
-          <div class="column second-name input-group" label="Last name">
-            <input class="input-box" type="text">
-          </div>
-        </div>
-        <div class="email input-group" label="Email">
-          <input class="input-box" type="text">
-        </div>
-        <div class="social input-group" label="Social accounts (Facebook, Instagram, Twitter, Steemit, etc.)">
-          <input class="input-box" type="text">
-        </div>
-        <div class="phone input-group" label="Phone">
-          <input class="input-box phone-box" type="text" maxlength="3" @keyup="phoneValidator($event, 3, 0)">
-          <input class="input-box phone-box" type="text" maxlength="3" @keyup="phoneValidator($event, 3, 1)">
-          <input class="input-box phone-box" type="text" maxlength="4" @keyup="phoneValidator($event, 4, 2)">
-        </div>
-        <div class="express input-group" label="What three links express you best?">
-          <input class="input-box" type="text">
-        </div>
-        <div class="accomplishment input-group" label="What creative accomplishment are you most pround of?">
-          <input class="input-box" type="text">
-        </div>
-        <div class="contribute input-group" label="With what do you see yourself contributing to Node?">
-          <input class="input-box" type="text">
-        </div>
-        <div><input type="submit" class="submit-btn" value="SUBMIT"></div>
-      </form> -->
     </div>
     <div class="footer">Product of <a href="https://byzantine.network"><span class="highlight">Byzantine</span></a> 🦄 </div>
     <div class="policies">
-        <!-- <a href="https://app.termly.io/document/terms-of-use-for-saas/c434d77d-667b-4b76-bb03-a69230544d07" target="_blank"><span class="highlight">Terms of Service</span></a> & <a href="https://app.termly.io/document/privacy-policy-for-website/a84c7ecf-d07a-47bd-b714-ff63bab2f3ab" target="_blank"><span class="highlight">Privacy Policy</span></a> -->
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -172,7 +297,280 @@ export default {
 </script>
 
 <style scoped>
-.landing{
+
+.landing {
+  /* height: 1000vh; */
+  background-image: url('~/assets/background.png');
+  background-size: 100% auto;
+  background-position: 0 40vw;
+  background-repeat: no-repeat;
+}
+.banner {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  height: 58vw;
+  background: #e5e5fa;
+}
+.banner-image {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  opacity: .5;
+}
+.page-title {
+  position: absolute;
+  top: 28vw;
+  width: 100%;
+  background: linear-gradient(180deg, #00eeff 25%, #1510ff, #ff00f6 78%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  font-family: "arame-regular", sans-serif;
+  font-size: 16vw;
+  color: rgba(255, 255, 255, .1);
+  letter-spacing: .4em;
+  text-align: center;
+  transform: translateY(-50%);
+}
+
+.welcome-unicorn,
+.explaining-unicorn,
+.device-unicorn {
+  position: relative;
+  margin-top: 8%;
+  text-align: center;
+}
+.explaining-unicorn {
+  margin-top: 30%;
+}
+.device-unicorn {
+  margin-top: 20%;
+}
+
+
+.unicorn-wrap {
+  position: absolute;
+  bottom: 45%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 15%;
+  filter: drop-shadow(0 0 20px rgba(0, 231, 250, .8));
+}
+
+.unicorn {
+  width: 100%;
+}
+.cloud {
+  width: 40%;
+}
+
+.dialog-box {
+  position: absolute;
+  top: 20%;
+  left: 100%;
+  margin-left: 40px;
+  padding: 16px 20px;
+  width: 304px;
+  background: rgba(250, 250, 245, .5);
+  text-align: left;
+  border: 1px solid #212121;
+  color: #212121;
+  letter-spacing: .1em;
+}
+.dialog-name {
+  font-weight: 500;
+  font-family: "arame-regular", sans-serif;
+}
+.dialog-p {
+  margin-top: 16px;
+  font-family: 'Roboto Mono', monospace;
+}
+
+.explaining-unicorn .dialog-box {
+  top: 0;
+  left: auto;
+  right: 100%;
+  margin: 0 40px 0 0;
+}
+
+.cloud-layer {
+  margin-top: 8%;
+}
+.cloud-single {
+  margin-left: 10%;
+  width: 25%;
+}
+.device-unicorn .cloud-single {
+  position: absolute;
+  margin-left: 10px;
+  height: 60px;
+  width: auto;
+
+}
+
+.hexagons {
+  position: relative;
+  margin-top: -200px;
+  margin-left: -20px;
+  padding: 30px 0;
+  width: 270px;
+  height: 270px;
+  text-align: center;
+  color: #f86bcf;
+  z-index: 2;
+}
+.hexagons .outer,
+.hexagons .inner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: #f86bcf;
+  -webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+  z-index: -1;
+}
+.hexagons .inner {
+  top: 3px;
+  left: 3px;
+  background: yellow;
+  height: 264px;
+  width: 264px;
+}
+.hexagons-title {
+  margin: 20px 0;
+  font-family: "arame-regular", sans-serif;
+} 
+.hexagons-text {
+  margin: 20px 0;
+  font-family: 'Roboto Mono', monospace;
+}
+
+.unicorn-with-whale,
+.unicorn-with-human,
+.unicorn-with-other,
+.unicorn-with-rothko {
+  display: flex;
+  margin: 24% auto 0;
+  max-width: 90%;
+}
+
+.unicorn-with-human,
+.unicorn-with-other,
+.unicorn-with-rothko {
+  margin-top: 20%;
+}
+
+.unicorn-with-whale .unicorn-wrap,
+.unicorn-with-human .unicorn-wrap,
+.unicorn-with-other .unicorn-wrap,
+.unicorn-with-rothko .unicorn-wrap {
+  flex: 0 0 auto;
+  position: static;
+  width: 15%;
+  transform: translateX(0);
+  filter: none;
+}
+.unicorn-with-whale .whale-wrap,
+.unicorn-with-human .whale-wrap,
+.unicorn-with-other .whale-wrap,
+.unicorn-with-rothko .whale-wrap  {
+  flex: 0 0 auto;
+  width: 20%;
+}
+.whale-wrap .whale {
+  margin-top: 20%;
+  width: 100%;
+}
+.unicorn-with-human .whale,
+.unicorn-with-other .whale,
+.unicorn-with-rothko .whale {
+  margin-top: 20%;
+  width: 80%;
+}
+
+
+.dialog-boxes .unicorn-box,
+.dialog-boxes .whale-box {
+  margin-left: 40px;
+  padding: 16px 20px;
+  width: 304px;
+  background: rgba(250, 250, 245, .5);
+  border: 1px solid #212121;
+  color: #212121;
+  letter-spacing: .1em;
+}
+.dialog-boxes .unicorn-box {
+  margin-right: 18vw;
+}
+.dialog-boxes .whale-box {
+  margin-top: 20px;
+  margin-left: 18vw;
+}
+
+.unicorn-with-team {
+  display: flex;
+  margin: 10% auto 0;
+  max-width: 90%;
+}
+
+.unicorn-with-team .unicorn-wrap {
+  flex: 0 0 auto;
+  position: static;
+  width: 30%;
+  transform: translateX(0);
+  filter: none;
+}
+.unicorn-with-team .unicorn {
+  margin-left: 18%;
+  width: 60%;
+}
+.unicorn-with-team .unicorn-box {
+  margin-left: 0;
+  margin-bottom: 10px;
+  padding: 16px 20px;
+  width: 100%;
+  background: rgba(250, 250, 245, .5);
+  border: 1px solid #212121;
+  color: #212121;
+  letter-spacing: .1em;
+}
+
+.unicorn-with-team .team-wrap {
+  padding-left: 40px;
+  margin-top: 10%;
+}
+.team-wrap img {
+  width: 100%;
+}
+
+.name-row {
+  margin: 10px 0;
+  display: flex;
+  text-align: center;
+}
+.name-row.left {
+  margin-left: 5%;
+}
+.name-column {
+  flex: 0 0 auto;
+  width: 24%;
+}
+.left .name-column {
+  width: 26%;
+}
+
+.team-member {
+  font-family: "arame-regular", sans-serif;
+}
+.team-desc {
+  margin: 0 auto;
+  font-family: 'Roboto Mono', monospace;
+}
+
+/* ************************* */
+/* .landing{
   background: #000;
 }
 .landing-header {
@@ -260,7 +658,6 @@ export default {
 }
 .apply-btn {
   padding: 13px 30px;
-  /* height: 50px; */
   background: #F58F10;
   border: none;
   font-family: "arame-regular", sans-serif;
@@ -281,40 +678,8 @@ export default {
 .apply-btn:active{
   background: #F58F10;
 }
-/* .typeform-btn {
-  padding: 13px 30px;
-  background: #F58F10;
-  border: none;
-  font-family: "arame-regular", sans-serif;
-  font-size: .9em;
-  text-decoration: none;
-  border-radius: 50px;
-  color: #fff;
-  letter-spacing: 2px;
-  outline: none;
-}
-.typeform-btn a{
-  text-decoration: none;
-  color: #fff;
-  letter-spacing: 2px;
-  outline: none;
-}
-.typeform-btn:hover{
-  background: #93005B;
-}
-.typeform-btn:active {
-  background: #D90086;
-} */
-/* .slogan {
-  padding: 100px 20px;
-  font-family: 'Archivo Black', sans-serif;
-  font-size: 2.4vw;
-  text-align: center;
-  letter-spacing: 2px;
-} */
 .highlight {
   text-decoration: none;
-  /* color: #ccc; */
 }
 
 .summary{
@@ -525,19 +890,19 @@ img.bike{
   opacity: 0;
 }
 
-::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+::-webkit-input-placeholder {
   color: #ccc;
   font-family: 'Roboto Mono', monospace;
 }
-::-moz-placeholder { /* Firefox 19+ */
+::-moz-placeholder {
   color: #ccc;
   font-family: 'Roboto Mono', monospace;
 }
-:-ms-input-placeholder { /* IE 10+ */
+:-ms-input-placeholder {
   color: #ccc;
   font-family: 'Roboto Mono', monospace;
 }
-:-moz-placeholder { /* Firefox 18- */
+:-moz-placeholder {
   color: #ccc;
   font-family: 'Roboto Mono', monospace;
 }
@@ -681,5 +1046,5 @@ img.bike{
     letter-spacing: .5em;
     margin-left: .5em;
   }
-}
+} */
 </style>
