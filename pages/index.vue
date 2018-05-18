ca723c039a55b6728afd809331cecfb7fec3e4ca
nadia <nadia@byzantine.network>
2018-05-18 15:07:24 -0400

    changed look

--- pages/index.vue
+++ pages/index.vue
@@ -211,8 +211,7 @@
 
     <div class="button-group">
       <div class="text">Check out our first quest here</div>
-      <button class="ready-btn">READY PLAYA?</button>
-      <a href="https://byzantine.network/spacequest/getspace/" class="ready-btn">Link Button</a>
+      <button href="https://byzantine.network/spacequest/getspace/" class="ready-btn">READY PLAYA?</button>
     </div>
 
     <footer class="footer">
